2aaa974bda289c20493e9aabc08f2a95390a3f01
Duwan Sierra <desierrag@gmail.com>
2023-05-18 21:11:06 -0500

    fix: url image

--- src/components/react/ImageComponent.tsx
+++ src/components/react/ImageComponent.tsx
@@ -6,7 +6,7 @@ const ImageComponent:React.FC = () => {
     <>
         <div className='basis-2/4'>
             <img 
-                src="./finance.svg"
+                src="finance.svg"
                 className='w-3/4 mx-auto animate-pulse animate-infinite animate-duration-[10000ms] animate-ease-in-out'
             />
         </div>
